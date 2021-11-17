0e1a689fc44d69e3e64ac8e96b0e7bb51bad0ece
Fatih Mehmet <fatihmd47@gmail.com>
2021-11-17 14:55:37 +0300

    Create no_glow_scroll_effect.dart

---
+++ no_glow_scroll_effect.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NoGlowScroll extends StatelessWidget {
+  const NoGlowScroll({Key? key, required this.child}) : super(key: key);
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<OverscrollIndicatorNotification>(
+      onNotification: (OverscrollIndicatorNotification overscroll) {
+        overscroll.disallowGlow();
+        return true;
+      },
+      child: child,
+    );
+  }
+}
